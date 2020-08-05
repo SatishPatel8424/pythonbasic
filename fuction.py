63b09ed940fdff08b4477e4e350d8d940205565f
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-05 16:38:00 +0530

    function example

--- fuction.py
+++ fuction.py
@@ -10,8 +10,8 @@ print(absolute_value(2))
 
 print(absolute_value(-4))
 # output :
-#2
-#4
+'''2
+4'''
 """ 2) Scope and Lifetime of variables """
 def my_func():
 	x = 10
@@ -64,4 +64,51 @@ Output: Hello Monica
 Hello Luke
 Hello Steve
 Hello John'''
+
+'''6) Example of a recursive function '''
+
+def factorial(x):
+
+    if x == 1:
+        return 1
+    else:
+        return (x * factorial(x-1))
+
+
+num = 4
+print("The factorial of", num, "is", factorial(num))
+''' The factorial of 4 is 24 '''
+
+'''7) Example of Lambda Function in python'''
+
+double = lambda x: x * 2
+
+print(double(5))
+
+''' Output: 10 '''
+
+''' 8) Example use with filter() '''
+my_list = [1, 5, 4, 6, 8, 11, 3, 12]
+
+new_list = list(filter(lambda x: (x%2 == 0) , my_list))
+
+print(new_list)
+
+''' Output : [4, 6, 8, 12] '''
+
+''' 9) Global variable and Local variable with same name'''
+
+x = 5
+def foo():
+    x = 10
+    print("local x:", x)
+
+
+foo()
+print("global x:", x)
+
+'''OutPut: local x: 10
+            global x: 5'''
+
+
 
