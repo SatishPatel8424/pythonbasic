f33c0e715a6b8b6232040aabb31ff5e5de851357
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-05 18:49:36 +0530

    python class example

--- class.py
+++ class.py
@@ -32,7 +32,6 @@ class Employee:
     def displayEmployee(self):
         print("Name : ", self.name, ", Salary: ", self.salary)
 
-
 "This would create first object of Employee class"
 emp1 = Employee("Zara", 2000)
 "This would create second object of Employee class"
@@ -40,6 +39,10 @@ emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
 print("Total Employee %d" % Employee.empCount)
+
+'''OutPut: Name :  Zara , Salary:  2000
+                    Name :  Manni , Salary:  5000
+                    Total Employee 2'''
 
 '''3) Class Inheritance example'''
 
@@ -71,3 +74,30 @@ c.childMethod()
 c.parentMethod()
 c.setAttr(200)
 c.getAttr()
+
+
+''' Output: Calling child constructor
+            Calling child method
+            Calling parent method
+            Parent attribute : 200'''
+
+'''4) Overloading Operators '''
+
+
+class Vector:
+    def __init__(self, a, b):
+        self.a = a
+        self.b = b
+
+    def __str__(self):
+        return 'Vector (%d, %d)' % (self.a, self.b)
+
+    def __add__(self, other):
+        return Vector(self.a + other.a, self.b + other.b)
+
+
+v1 = Vector(2, 10)
+v2 = Vector(5, -2)
+print (v1 + v2)
+
+'''Output: Vector (7, 8) '''
