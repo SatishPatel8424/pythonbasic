88fb36e64d47f37d1955e04553929f0ddc572f4a
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-05 17:35:16 +0530

    python class example

---
+++ class.py
@@ -0,0 +1,42 @@
+'''1) Defining a Class in Python'''
+
+class Person:
+    "This is a person class"
+    age = 10
+
+    def greet(self):
+        print('Hello')
+
+
+# Output: 10
+print(Person.age)
+
+# Output: 'This is my second class'
+print(Person.__doc__)
+
+''' 2) class example'''
+
+
+class Employee:
+
+    empCount = 0
+
+    def __init__(self, name, salary):
+        self.name = name
+        self.salary = salary
+        Employee.empCount += 1
+
+    def displayCount(self):
+        print("Total Employee %d" % Employee.empCount)
+
+    def displayEmployee(self):
+        print("Name : ", self.name, ", Salary: ", self.salary)
+
+
+"This would create first object of Employee class"
+emp1 = Employee("Zara", 2000)
+"This would create second object of Employee class"
+emp2 = Employee("Manni", 5000)
+emp1.displayEmployee()
+emp2.displayEmployee()
+print("Total Employee %d" % Employee.empCount)
