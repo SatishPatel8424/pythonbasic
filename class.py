5294885fdc4b5bc19bc5efea3048c7ee628f406d
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-05 17:49:13 +0530

    python class example

--- class.py
+++ class.py
@@ -40,3 +40,34 @@ emp2 = Employee("Manni", 5000)
 emp1.displayEmployee()
 emp2.displayEmployee()
 print("Total Employee %d" % Employee.empCount)
+
+'''3) Class Inheritance example'''
+
+class Parent:
+
+   parentAttr = 100
+   def __init__(self):
+      print("Calling parent constructor")
+
+   def parentMethod(self):
+      print('Calling parent method')
+
+   def setAttr(self, attr):
+      Parent.parentAttr = attr
+
+   def getAttr(self):
+      print("Parent attribute :", Parent.parentAttr)
+
+class Child(Parent):
+
+   def __init__(self):
+      print ("Calling child constructor")
+
+   def childMethod(self):
+      print('Calling child method')
+
+c = Child()
+c.childMethod()
+c.parentMethod()
+c.setAttr(200)
+c.getAttr()
