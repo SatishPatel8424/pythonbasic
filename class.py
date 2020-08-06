1f19e5f16ddfc4bfba30c206eab933eaefe8b2cc
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-06 10:23:44 +0530

    python class example

--- class.py
+++ class.py
@@ -122,3 +122,77 @@ class Derived(Base):
 obj1 = Base()
 print(obj1.a)
 ''' Output: satish'''
+
+''' 6) class example'''
+
+class Car(object):
+    condition = "new"
+    def __init__(self,model,color,mpg):
+        self.model=model
+        self.color=color
+        self.mpg=mpg
+my_car=Car("swift","silver",88)
+print(my_car.condition)
+
+'''Output: new'''
+
+'''7) class example'''
+class Car(object):
+    condition = "new"
+    def __init__(self, model, color, mpg):
+         self.model = model
+         self.color = color
+         self.mpg   = mpg
+
+    def display_car(self):
+       return "This is a %s %s with %s MPG." % (self.color,self.model,str(self.mpg))
+
+    def drive_car(self):
+        self.condition="used"
+
+class ElectricCar(Car):
+    def __init__(self,model, color, mpg, battery_type):
+        self.model = model
+        self.color = color
+        self.mpg   = mpg
+        self.battery_type=battery_type
+
+    def drive_car(self):
+        self.condition="like new"
+
+my_car=ElectricCar("swif", "silver", 88,"molten salt")
+print (my_car.condition)
+''' Output:new'''
+my_car.drive_car()
+print (my_car.condition)
+'''Output:like new'''
+
+'''class example'''
+
+
+class C:
+    counter = 0
+
+    def __init__(self):
+        type(self).counter += 1
+
+    def __del__(self):
+        type(self).counter -= 1
+
+
+if __name__ == "__main__":
+    x = C()
+    print("Number of instances: : " + str(C.counter))
+    y = C()
+    print("Number of instances: : " + str(C.counter))
+    del x
+    print("Number of instances: : " + str(C.counter))
+    del y
+    print("Number of instances: : " + str(C.counter))
+
+''' output:
+            Number of instances: : 1
+            Number of instances: : 2
+            Number of instances: : 1
+            Number of instances: : 0'''
+
