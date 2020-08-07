456f298d8ebb0a3a1a424e6b6eca6a4b698442d1
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-07 11:01:19 +0530

    python class example

--- fuction.py
+++ fuction.py
@@ -153,3 +153,21 @@ Country is Wakanda
 Age is 25
 Phone is 9876543210'''
 
+
+
+''' Functions as Parameters'''
+
+
+def g():
+    print("Hi, it's me 'g'")
+    print("Thanks for calling me")
+
+
+def f(func):
+    print("Hi, it's me 'f'")
+    print("I will call 'func' now")
+    func()
+
+
+f(g)
+
