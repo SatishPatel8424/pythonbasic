dd611e039d621550ffa2ea8bb069e2ffa88667ce
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-08 16:31:57 +0530

    python class example

--- class.py
+++ class.py
@@ -385,6 +385,40 @@ print(human.to_fahrenheit())
 572.0'''
 
 
+# Python program showing the use of
+# @property
+
+class Geeks:
+    def __init__(self):
+        self._age = 0
+
+
+    @property
+    def age(self):
+        print("getter method called")
+        return self._age
+
+    # a setter function
+    @age.setter
+    def age(self, a):
+        if (a < 18):
+            raise ValueError("Sorry you age is below eligibility criteria")
+        print("setter method called")
+        self._age = a
+
+
+mark = Geeks()
+
+mark.age = 19
+
+print(mark.age)
+''' Output:
+setter method called
+getter method called
+19'''
+
+
+
 
 
 
