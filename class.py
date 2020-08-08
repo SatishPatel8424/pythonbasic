9b834349cd6d56b909e3637e9382fddc1cda1d5c
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-08 09:47:10 +0530

    python class example

--- class.py
+++ class.py
@@ -345,33 +345,44 @@ getter method called
 10
 
     '''
+'''
+Using Getters and Setters'''
 
-# Using @property decorator
 class Celsius:
     def __init__(self, temperature=0):
-        self.temperature = temperature
+        self.set_temperature(temperature)
 
     def to_fahrenheit(self):
-        return (self.temperature * 1.8) + 32
+        return (self.get_temperature() * 1.8) + 32
 
-    @property
-    def temperature(self):
-        print("Getting value...")
+    # getter method
+    def get_temperature(self):
         return self._temperature
 
-    @temperature.setter
-    def temperature(self, value):
-        print("Setting value...")
+    # setter method
+    def set_temperature(self, value):
         if value < -273.15:
-            raise ValueError("Temperature below -273 is not possible")
+            raise ValueError("Temperature below -273.15 is not possible.")
         self._temperature = value
 
 
-# create an object
+# Create a new object, set_temperature() internally called by __init__
 human = Celsius(37)
 
-print(human.temperature)
+# Get the temperature attribute via a getter
+print(human.get_temperature())
 
+# Get the to_fahrenheit method, get_temperature() called by the method itself
 print(human.to_fahrenheit())
 
-coldest_thing = Celsius(-300)
+# new constraint implementation
+human.set_temperature(-300)
+
+# Get the to_fahreheit method
+print(human.to_fahrenheit())
+'''Output:37
+98.60000000000001
+Traceback (most recent call last):
+  File "<string>", line 30, in <module>
+  File "<string>", line 16, in set_temperature
+ValueError: Temperature below -273.15 is not possible.'''
