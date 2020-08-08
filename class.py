ae3f324136c2b69f234271bcfeb52e6f89c7efcb
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-08 16:20:14 +0530

    python class example

--- class.py
+++ class.py
@@ -386,36 +386,5 @@ print(human.to_fahrenheit())
 
 
 
-# Using property() function to achieve getters and setters behaviour
-
-class Geeks:
-    def __init__(self):
-        self._age = 0
-
-    # function to get value of _age
-    def get_age(self):
-        print("getter method called")
-        return self._age
-
-    # function to set value of _age
-    def set_age(self, a):
-        print("setter method called")
-        self._age = a
-
-    # function to delete _age attribute
-    def del_age(self):
-        del self._age
-
-    age = property(get_age, set_age, del_age)
 
 
-mark = Geeks()
-
-mark.age = 10
-
-print(mark.age)
-
-'''Output: 
-setter method called
-getter method called
-10'''
