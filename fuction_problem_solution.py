fe822a8ba4d6096061b4bf36d95fcab3f5cd6079
SatishPatel8424 <satishkotadiya2013@gmail.com>
2020-08-09 17:48:41 +0530

    python fuction problem and solution

--- fuction_problem_solution.py
+++ fuction_problem_solution.py
@@ -1,103 +1,60 @@
 #  function practice problems and solutions
 
 # problem: 1) Write a Python function using find max number
-from fuction import num
-
-
 def maximum(a, b, c):
     list = [a, b, c]
     return max(list)
-
 a = 10
 b = 14
 c = 12
-print("maximum number is : " +str(maximum(a, b, c)))
+
 '''output: maximum number is : 14'''
 
 # problem: 2) sum all the numbers in a list. lst:(6,8,6,7,8)
-
 def sum_num(num):
     total=0
     for x in num:
         total += x
     return total
-print("sum of list : " + str(sum_num((6, 8, 6, 7, 8))) )
 
 '''OutPut: sum of list : 35'''
 
 # problem: 3) python reverse a string using function. String : "1234satish"
-def Reverse_string(x):
+def reverse_string(x):
   return x[::-1]
 
-String = Reverse_string("1234satish")
-
-print(String)
+String = reverse_string("1234satish")
 
 # problem : 4) Write a Python function to calculate the factorial of a number (a non-negative integer).
-
 def fact(n):
     if n == 0:
         return 1
     else:
         return n * fact(n-1)
-n=int(input("enter the number:"))
-print(fact(n))
 
 # problem : 5)Define a function which can print a dictionary where the keys are numbers between 1 and 5 (both included) and the values are square of keys.
-
 def printDict():
     d = dict()
     for i in range(1, 4):
         d[i] = i ** 2
     print(d)
 
-
-printDict()
 '''
 output: {1: 1, 2: 4, 3: 9}
 '''
 
-# Problem 6) make a chain of function decorators (bold, italic, underline etc.).
-def bold(function):
-    def apply():
-        return "<b>" + function() + "</b>"
-    return apply
-
-def italic(function):
-    def apply():
-        return "<i>" + function() + "</i>"
-    return apply
-
-def underline(function):
-    def apply():
-        return "<u>" + function() + "</u>"
-    return apply
-@bold
-@italic
-@underline
-def test():
-    return "satish patel"
-print(test())
-
 # problem 7) Python program to access a function inside a function.
 def fuction_s(msg):
     def inside_f():
-
         print(msg)
-
     return inside_f
 
-
-
-another = fuction_s("satish")
-another()
+def another():
+    another = fuction_s("satish")
 
 # problem : 8) Write a Python program to detect the number of local variables declared in a function.
-
 def demo():
     x = 1
     y = 2
     str1= "satish"
     print("satish patel")
-
-print(demo.__code__.co_nlocals)
---
+++ main.py
@@ -0,0 +1,22 @@
+import sys
+
+sys.path.append('/home/satish/PycharmProjects/pythonbasic/')
+
+import fuction_problem_solution
+
+# call the function
+fuction_problem_solution.printDict()
+
+fuction_problem_solution.another()
+fuction_problem_solution.n = int(input("enter the number:"))
+
+
+# print the variable
+print("maximum number is : " + str(fuction_problem_solution.maximum(fuction_problem_solution.a,
+																	fuction_problem_solution.b,
+																	fuction_problem_solution.c)))
+print("sum of list : " + str(fuction_problem_solution.sum_num((6, 8, 6, 7, 8))))
+print("print the reverse string : " + fuction_problem_solution.String)
+print("factorial is : "+str(fuction_problem_solution.fact(fuction_problem_solution.n)))
+print(fuction_problem_solution.demo.__code__.co_nlocals)
+
